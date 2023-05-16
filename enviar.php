2b7941398e8d041f958687b2c9f9452d1dfc72d6
Krawa023 <132623378+Krawa023@users.noreply.github.com>
2023-05-16 13:36:36 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,27 +1,27 @@
 <?php
 $Nombre = $_POST['Nombre'];
 $Apellido = $_POST['Apellido'];
-$Edad = $_POST['Edad']
-$ComidaFavorita = $_POST['Comida Favorita']
-$AnimalFavorito = $_POST['Animal Favorito']
-$mail = $_POST['Email']
+$Edad = $_POST['Edad'];
+$ComidaFavorita = $_POST['Comida Favorita'];
+$AnimalFavorito = $_POST['Animal Favorito'];
+$Mail = $_POST['Email'];
 
 
-$header = 'From: ' . $mail . " \r\n";
+$header = 'From: ' . $Mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 $message = "Este mensaje fue enviado por: " . $Nombre . " " . $Apellido . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Su e-mail es: " . $Mail . " \r\n";
 $message .= "Su edad es: " . $Edad . " \r\n";
 $message .= "Su comida favorita es: " . $ComidaFavorita . " \r\n";
 $message .= "Su animal favorito es: " . $AnimalFavorito . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
-$para = 'krawabusiness@gmail.com';
-$asunto = 'Respuesta de MI PRIMER SITIO WEB';
+$Para = 'krawabusiness@gmail.com';
+$Asunto = 'Respuesta de MI PRIMER SITIO WEB';
 
-mail($para, $asunto, utf8_decode($message), $header);
+mail($Para, $Asunto, utf8_decode($message), $header);
 
 header("Location:index.html");
 ?>
